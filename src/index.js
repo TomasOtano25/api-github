f40f025c770cdf8c98f9bc23263e5dc0db8b8f1a
TomasOtano25 <35155721+TomasOtano25@users.noreply.github.com>
2018-03-03 18:26:01 -0400

    ListComponent

--- src/index.js
+++ src/index.js
@@ -7,16 +7,48 @@ import style from "./styles.css";
   textAlign: "center"
 };*/
 
+// http://placehold.it/75
+
 const Card = props => {
+  console.log(props.avatar);
   return (
-    <div className="card">
-      <img src="http://placehold.it/75" />
-      <div className="info">
-        <div>Name here ...</div>
-        <div>Company Name here ...</div>
+    <div style={{ margin: "1rem" }}>
+      <img width="75" src={props.avatar_url} />
+      <div style={{ display: "inline-block", marginLeft: 10 }}>
+        <div style={{ fontSize: "1.2rem", fontWeight: "bold" }}>
+          {props.name}
+        </div>
+        <div>{props.company}</div>
       </div>
     </div>
   );
 };
 
-ReactDom.render(<Card />, document.getElementById("root"));
+let data = [
+  {
+    name: "Paul O’Shannessy",
+    avatar_url: "https://avatars1.githubusercontent.com/u/8445?v=4",
+    company: "Facebook"
+  },
+  {
+    name: "null",
+    avatar_url: "https://avatars2.githubusercontent.com/u/35155721?v=4",
+    company: "null"
+  }
+];
+
+const CardList = props => {
+  return (
+    <div>
+      {props.cards.map(card => (
+        <Card
+          name={card.name}
+          avatar_url={card.avatar_url}
+          company={card.company}
+        />
+      ))}
+    </div>
+  );
+};
+
+ReactDom.render(<CardList cards={data} />, document.getElementById("root"));
