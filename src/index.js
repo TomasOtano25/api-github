5c91a0df476970b3c64c2d9f127cddd45cbdcce7
TomasOtano25 <35155721+TomasOtano25@users.noreply.github.com>
2018-03-04 17:47:56 -0400

    Listo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDom, { render } from "react-dom";
 import style from "./styles.css";
+import axios from "axios";
 
 /*const styles = {
   fontFamily: "sans-serif",
@@ -24,19 +25,6 @@ const Card = props => {
   );
 };
 
-let data = [
-  {
-    name: "Paul O’Shannessy",
-    avatar_url: "https://avatars1.githubusercontent.com/u/8445?v=4",
-    company: "Facebook"
-  },
-  {
-    name: "null",
-    avatar_url: "https://avatars2.githubusercontent.com/u/35155721?v=4",
-    company: "null"
-  }
-];
-
 const CardList = props => {
   return (
     <div>
@@ -53,10 +41,37 @@ const CardList = props => {
 };
 
 class Form extends React.Component {
+  state = { userName: "" };
+  // Cada funcion de evento en react recibe un argumento event
+  handleSubmit = event => {
+    // Su funcion es hacer que el formulario no actualize la pagina
+    event.preventDefault();
+    // console.log("Event: Form Submit", this.userNameInput.value);
+    // ajax ... {fecth or axios}
+    axios
+      .get(`https://api.github.com/users/${this.state.userName}`)
+      .then(resp => {
+        // console.log(resp);
+        this.props.onSubmit(resp.data);
+        this.setState({ userName: "" });
+      });
+  };
+
   render() {
     return (
-      <form>
-        <input type="text" placeholder="Github username" />
+      <form onSubmit={this.handleSubmit}>
+        <input
+          value={this.state.userName}
+          onChange={event => {
+            this.setState({
+              userName: event.target.value
+            });
+          }}
+          type="text"
+          placeholder="Github username"
+          required
+          // ref={input => (this.userNameInput = input)}
+        />
         <button type="submit">Add card</button>
       </form>
     );
@@ -64,11 +79,34 @@ class Form extends React.Component {
 }
 
 class App extends React.Component {
+  state = {
+    cards: [
+      /*{
+        name: "Paul O’Shannessy",
+        avatar_url: "https://avatars1.githubusercontent.com/u/8445?v=4",
+        company: "Facebook"
+      },
+      {
+        name: "fdds",
+        avatar_url: "https://avatars2.githubusercontent.com/u/35155721?v=4",
+        company: "gyg"
+      }*/
+    ]
+  };
+
+  addNewCard = cardInfo => {
+    // console.log(cardInfo);
+
+    this.setState(prevState => ({
+      cards: prevState.cards.concat(cardInfo)
+    }));
+  };
+
   render() {
     return (
       <div>
-        <Form />
-        <CardList cards={data} />
+        <Form onSubmit={this.addNewCard} />
+        <CardList cards={this.state.cards} />
       </div>
     );
   }
